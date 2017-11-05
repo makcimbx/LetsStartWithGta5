9c2fef903035c4422fd0eefd47e75264c3f1ea4a
makcimbx <makcimbx@gmail.com>
2017-11-05 16:16:41 +0300

    1

--- server/server-data/resources/[system]/settime/sv_settime.lua
+++ server/server-data/resources/[system]/settime/sv_settime.lua
@@ -1,6 +1,6 @@
 AddEventHandler('chatMessage', function(player, color, message)
     if message:sub(1, 5) == '/time' then
 	    hour = message:sub(7, 8)
-        TriggerClientEvent('SetTimeEvent', -1, hour)
+        TriggerClientEvent('SetTimeEvent', player, hour)
     end
 end)
